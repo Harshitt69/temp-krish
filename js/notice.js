55e5f3d0e0822bb3b5fa68ab182eb1837879c049
Harshit Anand <harshitanand64.ha@gmail.com>
2021-12-24 02:46:31 +0530

    added attractions pages

---
+++ js/notice.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $('#news-slider').owlCarousel({
+    items: 4,
+    itemsDesktop: [
+      1199,
+      4
+    ],
+    itemsDesktopSmall: [
+      980,
+      3
+    ],
+    itemsMobile: [
+      740,
+      2
+    ],
+    itemsMobile: [
+      500,
+      1
+    ],
+    navigation: true,
+    navigationText: [
+      '',
+      ''
+    ],
+    pagination: true,
+    autoPlay: true
+  });
+});
